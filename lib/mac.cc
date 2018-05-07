e7f21e6015017d0f8e7364691e2eb8edf00c7921
David Zwart <davidzwa@gmail.com>
2018-05-07 16:54:58 +0200

    Code formatting

--- lib/mac.cc
+++ lib/mac.cc
@@ -174,7 +174,9 @@ private:
 };
 
 mac::sptr
-mac::make(std::vector<uint8_t> src_mac, std::vector<uint8_t> dst_mac, std::vector<uint8_t> bss_mac) {
+mac::make(std::vector<uint8_t> src_mac, 
+			std::vector<uint8_t> dst_mac, 
+			std::vector<uint8_t> bss_mac) {
 	return gnuradio::get_initial_sptr(new mac_impl(src_mac, dst_mac, bss_mac));
 }
 
